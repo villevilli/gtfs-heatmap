043a12749e293a2bfa1ada05b5d4d0ea10d48ad4
ville <kujala.ville.vk@gmail.com>
2024-10-14 20:25:12 +0300

    removed old leftover functions

--- gtfs_heatmap_lib/src/lib.rs
+++ gtfs_heatmap_lib/src/lib.rs
@@ -4,13 +4,9 @@ pub mod coords;
 pub mod gtfs_graph;
 pub mod gtfs_types;
 
+use gtfs_structures::Gtfs;
 use std::sync::Arc;
 
-use coords::Coordinates;
-use futures::executor;
-pub use gtfs_structures::Gtfs;
-use gtfs_structures::StopTime;
-use gtfs_types::{Day, StopTrip};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -21,57 +17,4 @@ pub enum Error {
 
 pub async fn get_stops(gtfs_data: &Gtfs) -> Vec<&Arc<gtfs_structures::Stop>> {
     gtfs_data.stops.values().collect()
-}
-
-pub async fn get_stop_connection(
-    gtfs_data: &Gtfs,
-) -> Result<Vec<&Arc<gtfs_structures::Stop>>, Error> {
-    todo!();
-}
-
-pub async fn get_nearby_stops(
-    gtfs_data: &Gtfs,
-    coords: &Coordinates,
-    search_box_distance: &f64,
-) -> Result<Vec<gtfs_types::Stop>, Error> {
-    todo!();
-}
-
-pub fn get_next_stop_sync(
-    trip_id: &String,
-    stop_sequence: &u32,
-    gtfs_data: &Gtfs,
-) -> Result<StopTrip, Error> {
-    let next_stop = get_next_stop(trip_id, stop_sequence, gtfs_data);
-    executor::block_on(next_stop)
-}
-
-pub async fn get_next_stop(
-    trip_id: &String,
-    stop_sequence: &u32,
-    gtfs_data: &Gtfs,
-) -> Result<StopTrip, Error> {
-    todo!();
-}
-
-pub fn get_next_stops_by_time<'a>(
-    time: u32,
-    day: &Day,
-    stop_id: &String,
-    gtfs_data: &'a Gtfs,
-) -> Vec<&'a StopTime> {
-    gtfs_data
-        .trips
-        .values()
-        .map(|trip| {
-            trip.stop_times.iter().filter(|stop_time| {
-                if &stop_time.stop.id == stop_id {
-                    true
-                } else {
-                    false
-                }
-            })
-        })
-        .flatten()
-        .collect()
 }
